f9c13e5f9a97022fbd2bc7eb09d93093f5b2579f
i.ra <i.ra@gxp.co.jp>
2020-01-10 14:38:04 +0900

    update

--- spring-batch-helloworld-h2/src/main/java/com/luo/springbatchhelloworldh2/SpringBatchHelloworldH2Application.java
+++ spring-batch-helloworld-h2/src/main/java/com/luo/springbatchhelloworldh2/SpringBatchHelloworldH2Application.java
@@ -1,13 +1,13 @@
-package com.luo.springbatch;
+package com.luo.springbatchhelloworldh2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbatchApplication {
+public class SpringBatchHelloworldH2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbatchApplication.class, args);
+		SpringApplication.run(SpringBatchHelloworldH2Application.class, args);
 	}
 
 }
--- spring-batch-helloworld-h2/src/test/java/com/luo/springbatchhelloworldh2/SpringBatchHelloworldH2ApplicationTests.java
+++ spring-batch-helloworld-h2/src/test/java/com/luo/springbatchhelloworldh2/SpringBatchHelloworldH2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luo.springbatch;
+package com.luo.springbatchhelloworldh2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbatchApplicationTests {
+class SpringBatchHelloworldH2ApplicationTests {
 
 	@Test
 	void contextLoads() {

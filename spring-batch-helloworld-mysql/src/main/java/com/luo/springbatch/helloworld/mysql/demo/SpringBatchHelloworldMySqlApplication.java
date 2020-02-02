08cd02f012f5b9feeff7c4158e4f612c4b659554
foxman0808 <35596110+foxman0808@users.noreply.github.com>
2020-02-03 01:31:26 +0900

    追加mysql

---
+++ spring-batch-helloworld-mysql/src/main/java/com/luo/springbatch/helloworld/mysql/demo/SpringBatchHelloworldMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.luo.springbatch.helloworld.mysql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchHelloworldMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchHelloworldMySqlApplication.class, args);
+	}
+
+}
